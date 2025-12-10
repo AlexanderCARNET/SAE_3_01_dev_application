0e61ef8eb9ea46462bda9db0c94e78c50bcdd775
BOUDOUAH-Ilias <ilias.boudouah8@etu.univ-lorraine.fr>
2025-12-10 15:10:12 +0100

    [TEST] Ajout du fichier test

---
+++ Ollert_QDEV/test/ColonneTest.java
@@ -0,0 +1,18 @@
+import Ollert.TacheComposite;
+import donnees.Colonne;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Date;
+
+public class ColonneTest {
+
+    @BeforeEach
+    public void setUp(){
+        Colonne c = new Colonne("test");
+    }
+
+    public void test_colonne_ajoutTache_OK(){
+
+    }
+
+}
