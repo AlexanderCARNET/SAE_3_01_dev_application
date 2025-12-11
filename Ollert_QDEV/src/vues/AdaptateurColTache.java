9f0302e933046edf6731cbcde25cc3c0741d2bff
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-12-11 11:50:14 +0100

    [ADDED] ajout du constructeur

--- Ollert_QDEV/src/vues/AdaptateurColTache.java
+++ Ollert_QDEV/src/vues/AdaptateurColTache.java
@@ -5,4 +5,10 @@ import donnees.Tache;
 public class AdaptateurColTache {
     private Tache tache;
     private String colonne;
+
+    public AdaptateurColTache(Tache tache, String colonne){
+        this.tache=tache;
+        this.colonne=colonne;
+    }
+
 }
