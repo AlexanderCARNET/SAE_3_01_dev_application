fca75b3298a9a716ffaf480d7a24f2eebef61c41
walid <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-12-11 14:35:20 +0100

    [FIXED] corection des type des objets + petits bugs

--- Ollert_QDEV/src/vues/VueListes.java
+++ Ollert_QDEV/src/vues/VueListes.java
@@ -1,56 +1,64 @@
 package vues;
 
+import donnees.Colonne;
 import donnees.Tache;
 
+import donnees.TacheComposite;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class VueListes extends TableView<Tache> implements StrategieModeAffichage{
+public class VueListes extends TableView<TacheComposite> implements StrategieModeAffichage{
+
+    private Map<TacheComposite, String> colonnes = new HashMap<>();
 
     public VueListes() {
+        super();
         this.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
-        TableColumn<Tache,String> colTache = new TableColumn<>("Tache");
+        TableColumn<TacheComposite,String> colTache = new TableColumn<>("Tache");
         colTache.setCellValueFactory(new PropertyValueFactory<>("titre"));
 
-        TableColumn<Tache,String> colCol = new TableColumn<>("Colonne");
-        colCol.setCellValueFactory(new PropertyValueFactory<>("colonne"));
+        TableColumn<TacheComposite,String> colCol = new TableColumn<>("Colonne");
+        colCol.setCellValueFactory(cellData -> {
+            TacheComposite tache = cellData.getValue();
+            String colonne = colonnes.get(tache);
+            return new SimpleStringProperty(colonne);
+        });
         colCol.setStyle("-fx-alignment: CENTER;");
 
-        TableColumn<Tache, Date> colDate = new TableColumn<>("Date debut");
+        TableColumn<TacheComposite, Date> colDate = new TableColumn<>("Date debut");
         colDate.setCellValueFactory(new PropertyValueFactory<>("dateDebut"));
         colDate.setStyle("-fx-alignment: CENTER;");
-        colDate.setCellFactory(column -> new TableCell<Tache,  Date>() {
+        colDate.setCellFactory(column -> new TableCell<TacheComposite, Date>() {
+            private final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy");
             @Override
             protected void updateItem(Date item, boolean empty) {
                 super.updateItem(item, empty);
-                if (empty || item == null) {
-                    setText(null);
-                }
-                else {
-                    setText(item.toString());
-                }
+                if (empty || item == null) setText(null);
+                else setText(format.format(item));
             }
         });
 
-        TableColumn<Tache, Integer> colDuree = new TableColumn<>("Duree");
+        TableColumn<TacheComposite, Integer> colDuree = new TableColumn<>("Duree");
         colDuree.setCellValueFactory(new PropertyValueFactory<>("duree"));
         colDuree.setStyle("-fx-font-size: 15px; -fx-alignment: CENTER;");
-        colDuree.setCellFactory(column -> new TableCell<Tache, Integer>() {
+        colDuree.setCellFactory(column -> new TableCell<TacheComposite, Integer>() {
             @Override
             protected void updateItem(Integer item, boolean empty) {
                 super.updateItem(item, empty);
-                if (empty || item == null) {
-                    setText(null);
-                } else {
-                    setText(item + " jours");
-                }
+                if (empty || item == null) setText(null);
+                else setText(item+"");
             }
         });
 
