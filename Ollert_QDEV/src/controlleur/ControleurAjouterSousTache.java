10b63e49bf698a2b9a262bfede1bc485901979e4
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2025-12-12 10:57:38 +0100

    [Feature] ControleurAjouterSousTache : Ajout du constructeur

---
+++ Ollert_QDEV/src/controlleur/ControleurAjouterSousTache.java
@@ -0,0 +1,13 @@
+package controlleur;
+
+import donnees.TacheComposite;
+
+public class ControleurAjouterSousTache  {
+
+    private final TacheComposite tacheParente;
+
+    public ControleurAjouterSousTache(TacheComposite parent) {
+        this.tacheParente = parent;
+    }
+
+}
