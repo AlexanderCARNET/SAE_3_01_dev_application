e3312c3044b2a931abe7f9d8984d83e6adc04085
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2025-12-12 10:21:46 +0100

    [Feature] ControleurAjouterTache : Ajout de la méthode ajouterTache()

--- Ollert_QDEV/src/controlleur/ControleurAjouterTache.java
+++ Ollert_QDEV/src/controlleur/ControleurAjouterTache.java
@@ -3,12 +3,26 @@ package controlleur;
 import donnees.Colonne;
 import donnees.Tache;
 
+import java.util.Date;
+
 public class ControleurAjouterTache {
 
     private final Colonne colonne;
 
-
     public ControleurAjouterTache(Colonne colonne) {
         this.colonne = colonne;
+    }
+
+    public boolean ajouterTache(String titre, String description, int duree, Date dateDebut) {
+
+        if (titre == null || titre.isBlank()) return false;
+        if (duree < 0) return false;
+        if (dateDebut == null) return false;
+
+        Tache t = new Tache(titre, description, duree, dateDebut);
+
+        colonne.ajouteTache(t);
+
+        return true;
     }
 }
