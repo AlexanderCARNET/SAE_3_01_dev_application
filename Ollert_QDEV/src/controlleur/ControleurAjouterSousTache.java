0190cd3adc209fe1965017525455a120c5d8ca9c
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2025-12-12 10:58:35 +0100

    [Feature] ControleurAjouterSousTache : Ajout de la methode ajouterSousTache()

--- Ollert_QDEV/src/controlleur/ControleurAjouterSousTache.java
+++ Ollert_QDEV/src/controlleur/ControleurAjouterSousTache.java
@@ -1,6 +1,9 @@
 package controlleur;
 
+import donnees.Tache;
 import donnees.TacheComposite;
+
+import java.util.Date;
 
 public class ControleurAjouterSousTache  {
 
@@ -9,5 +12,20 @@ public class ControleurAjouterSousTache  {
     public ControleurAjouterSousTache(TacheComposite parent) {
         this.tacheParente = parent;
     }
+
+
+    public boolean ajouterSousTache(String titre, String description, int duree, Date dateDebut) {
+
+        if (titre == null || titre.isBlank()) return false;
+        if (duree < 0) return false;
+        if (dateDebut == null) return false;
+
+        Tache nouvelle = new Tache(titre, description, duree, dateDebut);
+
+        tacheParente.ajouterSousTache(nouvelle);
+
+        return true;
+    }
+}
 
 }
