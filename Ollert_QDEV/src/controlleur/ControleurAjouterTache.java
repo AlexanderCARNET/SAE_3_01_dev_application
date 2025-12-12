9ae50714628e273ea077f2b28f9a73892b62130c
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2025-12-12 10:18:19 +0100

    [Feature] ControleurAjouterTache : Ajout du constructeur

--- Ollert_QDEV/src/controlleur/ControleurAjouterTache.java
+++ Ollert_QDEV/src/controlleur/ControleurAjouterTache.java
@@ -1,4 +1,14 @@
 package controlleur;
 
+import donnees.Colonne;
+import donnees.Tache;
+
 public class ControleurAjouterTache {
+
+    private final Colonne colonne;
+
+
+    public ControleurAjouterTache(Colonne colonne) {
+        this.colonne = colonne;
+    }
 }
