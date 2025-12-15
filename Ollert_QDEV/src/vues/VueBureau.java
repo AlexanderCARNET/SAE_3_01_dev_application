2bd72a51b852bb01016e9511d5ace9a1c86d1273
Alexander CARNET <e67005u@univ-lorraine.fr>
2025-12-15 17:18:05 +0100

    [INIT] creation de la classe VueBureau

---
+++ Ollert_QDEV/src/vues/VueBureau.java
@@ -0,0 +1,19 @@
+package vues;
+
+import donnees.Colonne;
+import donnees.Modele;
+
+import java.util.List;
+
+public class VueBureau implements StrategieModeAffichage{
+    @Override
+    public void genererAffichage(Modele model) {
+
+    }
+
+    @Override
+    public void actualiser(){
+
+
+    }
+}
