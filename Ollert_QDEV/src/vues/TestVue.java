ae41ffbbcc8c2fdde821e5e9ee4dd506db5edb65
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-12-15 15:55:50 +0100

    [Modification] adaptation model

--- Ollert_QDEV/src/vues/TestVue.java
+++ Ollert_QDEV/src/vues/TestVue.java
@@ -1,6 +1,7 @@
 package vues;
 
 import donnees.Colonne;
+import donnees.Modele;
 import donnees.Tache;
 import donnees.TacheComposite;
 import javafx.application.Application;
@@ -17,22 +18,24 @@ public class TestVue extends Application {
     @Override
     public void start(Stage stage){
         Date date = new Date();
-        List<Colonne> colonnes = new ArrayList<>();
 
         Colonne c1 = new Colonne("Fini");
         c1.ajouteTache(new Tache("Database Design", "Archi", 2, date));
-        colonnes.add(c1);
 
         Colonne c2 = new Colonne("en Cours");
         c2.ajouteTache(new Tache("ok", "Dev", 5, date));
         c2.ajouteTache(new Tache("Sleep", "Hobby", 5, date));
-        colonnes.add(c2);
 
         Colonne c3 = new Colonne("A faire");
         c3.ajouteTache(new Tache("Testing", "QA", 1, date));
-        colonnes.add(c3);
 
-        VueTaches vueTaches = new VueTaches(colonnes);
+        Modele model = new Modele();
+        model.ajouterColonne(c1);
+        model.ajouterColonne(c2);
+        model.ajouterColonne(c3);
+
+        VueTaches vueTaches = new VueTaches(model);
+        model.ajouterObservateur(vueTaches);
 
         VueListes vue = new VueListes();
 
