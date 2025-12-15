7a19102a70d83aca6141c467dae4f02d945433f4
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2025-12-15 14:15:54 +0100

    [Feature] Ajout du controleurModifierTache

---
+++ Ollert_QDEV/src/controlleur/ControleurModifierTache.java
@@ -0,0 +1,15 @@
+package controlleur;
+
+import donnees.TacheComposite;
+import java.util.Date;
+
+public class ControleurModifierTache {
+
+    private final TacheComposite tache;
+
+    public ControleurModifierTache(TacheComposite tache) {
+        this.tache = tache;
+    }
+
+
+}
