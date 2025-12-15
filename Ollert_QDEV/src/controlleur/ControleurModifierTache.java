6a8679869d181810392196cb1988ddb1f0ca3d5e
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2025-12-15 14:16:14 +0100

    [Feature] Ajout de la méthode modifierTache dans controleurModifierTache

--- Ollert_QDEV/src/controlleur/ControleurModifierTache.java
+++ Ollert_QDEV/src/controlleur/ControleurModifierTache.java
@@ -11,5 +11,17 @@ public class ControleurModifierTache {
         this.tache = tache;
     }
 
+    public boolean modifierTache(String titre, String description, int duree, Date dateDebut) {
 
+        if (titre == null || titre.isBlank()) return false;
+        if (duree < 0) return false;
+        if (dateDebut == null) return false;
+
+        tache.setTitre(titre);
+        tache.setDescription(description);
+        tache.setDuree(duree);
+        tache.setDateDebut(dateDebut);
+
+        return true;
+    }
 }
