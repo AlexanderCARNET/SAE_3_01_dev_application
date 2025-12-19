95dbdad3bbff9f21f411910fcaae3570d548461c
Alexander CARNET <e67005u@univ-lorraine.fr>
2025-12-19 09:23:18 +0100

    [UPDATE] retirer un print

--- Ollert_QDEV/src/controlleur/ControleurChangeTitreColonne.java
+++ Ollert_QDEV/src/controlleur/ControleurChangeTitreColonne.java
@@ -26,7 +26,6 @@ public class ControleurChangeTitreColonne implements EventHandler<MouseEvent> {
     @Override
     public void handle(MouseEvent mouseEvent) {
         if(mouseEvent.getClickCount() == 2){
-            System.out.println("deeededed");
             this.vueBureau.colonneEnModif = c;
             this.m.notifier();
         }
