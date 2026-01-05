b0c119de2ca36ac0c1bed416cf1bf03055d46ae6
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-01-05 14:49:16 +0100

    [Feature] Ajout de toute les informations a PopupDetailTache

--- Ollert_QDEV/src/vues/PopupDetailsTache.java
+++ Ollert_QDEV/src/vues/PopupDetailsTache.java
@@ -1,7 +1,10 @@
 package vues;
 import donnees.TacheComposite;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -21,6 +24,44 @@ public class PopupDetailsTache {
 
         Label lblTitre = new Label(tache.getTitre());
         lblTitre.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+
+        GridPane grid = new GridPane();
+        grid.setHgap(15);
+        grid.setVgap(12);
+
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setMinWidth(140);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setHgrow(javafx.scene.layout.Priority.ALWAYS);
+        grid.getColumnConstraints().addAll(col1, col2);
+
+        // Description
+        Label lblDescTitre = new Label("Description :");
+        lblDescTitre.setStyle("-fx-font-weight: bold;");
+
+        Label lblDescription = new Label(tache.getDescription());
+        lblDescription.setWrapText(true);
+        lblDescription.setMinHeight(90);
+        lblDescription.setAlignment(Pos.TOP_LEFT);
+        lblDescription.setStyle(
+                "-fx-background-color: white;" +
+                        "-fx-padding: 8;" +
+                        "-fx-border-color: #dddddd;"
+        );
+
+        Label lblDureeTitre = new Label("Durée :");
+        lblDureeTitre.setStyle("-fx-font-weight: bold;");
+        Label lblDuree = new Label(String.valueOf(tache.getDuree()));
+
+        Label lblDateTitre = new Label("Date de début :");
+        lblDateTitre.setStyle("-fx-font-weight: bold;");
+        Label lblDate = new Label(tache.getDateDebut().toString());
+
+        Label lblDepTitre = new Label("Dépendances :");
+        lblDepTitre.setStyle("-fx-font-weight: bold;");
+        Label lblDependances = new Label(
+                String.valueOf(tache.getDependances().size())
+        );
 
 
 
