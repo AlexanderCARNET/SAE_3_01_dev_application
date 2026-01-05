fc63cb187bf148c69bc6c8d63b943ee5328e02e2
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-01-05 14:45:52 +0100

    [Feature] Ajout de la Vbox a PopupDetailTache

--- Ollert_QDEV/src/vues/PopupDetailsTache.java
+++ Ollert_QDEV/src/vues/PopupDetailsTache.java
@@ -1,5 +1,7 @@
 package vues;
 import donnees.TacheComposite;
+import javafx.geometry.Insets;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -12,9 +14,14 @@ public class PopupDetailsTache {
         stage.setTitle("Détails de la tâche");
         stage.setResizable(false);
 
+        VBox root = new VBox(15);
+        root.setPadding(new Insets(20));
+        root.setStyle("-fx-background-color: #f7f7f7;");
+
+
 
 
     }
     }
 
-    }
+
