a94c1449ef51a4d367d5c962cc02fda628cba9d0
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-01-05 14:43:32 +0100

    [Feature] Ajout du Stage a PopupDetailTache

--- Ollert_QDEV/src/vues/PopupDetailsTache.java
+++ Ollert_QDEV/src/vues/PopupDetailsTache.java
@@ -1,9 +1,17 @@
 package vues;
 import donnees.TacheComposite;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class PopupDetailsTache {
 
     public void display(TacheComposite tache) {
+
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Détails de la tâche");
+        stage.setResizable(false);
+
 
 
     }
