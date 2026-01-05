d5331bd5c6a4d4bcab868852806fcd5a10a79238
Alexander CARNET <e67005u@univ-lorraine.fr>
2026-01-05 16:45:40 +0100

    [UPDATE] creation du controleur pour acceder à la selection de taches gantt

---
+++ Ollert_QDEV/src/controlleur/ControleurSelectionTache.java
@@ -0,0 +1,19 @@
+package controlleur;
+
+import donnees.Modele;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import vues.PopupSelectionTaches;
+
+public class ControleurSelectionTache implements EventHandler<ActionEvent> {
+
+    private Modele modele;
+    public ControleurSelectionTache(Modele modele) {
+        this.modele = modele;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        PopupSelectionTaches.display(modele);
+    }
+}
