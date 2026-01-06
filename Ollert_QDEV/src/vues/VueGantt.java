7a452a23592940eb2404f939b8f4a453f9a088e5
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-01-06 11:06:01 +0100

    [Feature] Ajout du début de l'affichage de Gantt

--- Ollert_QDEV/src/vues/VueGantt.java
+++ Ollert_QDEV/src/vues/VueGantt.java
@@ -17,6 +17,40 @@ public class VueGantt extends Pane implements StrategieModeAffichage {
     @Override
     public Pane genererAffichage(Modele modele) {
 
-        
+        this.getChildren().clear();
+
+        GanttCalculator calc = new GanttCalculator();
+
+        List<TacheComposite> toutesLesTaches =
+
+        List<GanttTask> gantt = calc.generer(toutesLesTaches);
+
+        if (gantt.isEmpty()) return this;
+
+        Date minDate = gantt.stream().map(GanttTask::getDebut).min(Date::compareTo).orElse(new Date());
+
+        int y = 20;
+
+        for (GanttTask gt : gantt) {
+
+            long offsetJour = (gt.getDebut().getTime() - minDate.getTime()) / (1000 * 60 * 60 * 24);
+
+            long dureeJour = (gt.getFin().getTime() - gt.getDebut().getTime()) / (1000 * 60 * 60 * 24);
+
+            Rectangle barre = new Rectangle(offsetJour * ECHELLE_JOUR, y, Math.max(dureeJour, 1) * ECHELLE_JOUR, 20);
+
+            barre.setFill(Color.DODGERBLUE);
+            barre.setArcHeight(5);
+            barre.setArcWidth(5);
+
+            Label label = new Label(gt.getTache().getTitre());
+            label.setLayoutX(barre.getX() + 5);
+            label.setLayoutY(y - 15);
+
+            this.getChildren().addAll(barre, label);
+            y += HAUTEUR_LIGNE;
+        }
+
+        return this;
     }
 }
