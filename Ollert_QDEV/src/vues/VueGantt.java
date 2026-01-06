f5c2ff5aafc315acc570367ea97ffe68b262b42a
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-01-06 11:02:59 +0100

    [Feature] Ajout de la méthode genererAffichage dans VUeGantt

--- Ollert_QDEV/src/vues/VueGantt.java
+++ Ollert_QDEV/src/vues/VueGantt.java
@@ -1,4 +1,22 @@
 package vues;
 
-public class VueGantt {
+import donnees.*;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.util.Date;
+import java.util.List;
+
+public class VueGantt extends Pane implements StrategieModeAffichage {
+
+    private static final int HAUTEUR_LIGNE = 30;
+    private static final int ECHELLE_JOUR = 20;
+
+    @Override
+    public Pane genererAffichage(Modele modele) {
+
+        
+    }
 }
