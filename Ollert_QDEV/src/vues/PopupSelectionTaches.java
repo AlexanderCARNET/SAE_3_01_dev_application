055ee4520b7287bcf6c3a3404931a6875c8e77f2
Alexander CARNET <e67005u@univ-lorraine.fr>
2026-01-06 11:18:51 +0100

    [UPDATE] ajout de bouton pour generer le diagramme de gantt et tout selectionner dans le poup selection taches pour gantt

--- Ollert_QDEV/src/vues/PopupSelectionTaches.java
+++ Ollert_QDEV/src/vues/PopupSelectionTaches.java
@@ -5,6 +5,7 @@ import controlleur.ControlleurSelectionnerTacheGantt;
 import donnees.Modele;
 import donnees.Tache;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -40,6 +41,17 @@ public class PopupSelectionTaches {
 
             principale.getChildren().add(ligneTache);
         }
+
+        HBox conteneurBarBas = new HBox();
+        conteneurBarBas.setSpacing(20);
+
+        Button boutonSelectionTout = new Button("Selection tout");
+        boutonSelectionTout.setMaxWidth(80);
+
+        Button bouton = new Button("générer Diagramme");
+        conteneurBarBas.getChildren().addAll(bouton, boutonSelectionTout);
+
+        principale.getChildren().addAll(conteneurBarBas);
 
         Scene scene = new Scene(principale);
         window.setScene(scene);
