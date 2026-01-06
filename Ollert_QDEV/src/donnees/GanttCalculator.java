88b3658428c076d7ce4d52aabbb1833fd4c7dfdc
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-01-06 10:41:09 +0100

    [Feature] Ajout de calculerDateDeut dans GanttCalculator

--- Ollert_QDEV/src/donnees/GanttCalculator.java
+++ Ollert_QDEV/src/donnees/GanttCalculator.java
@@ -7,4 +7,25 @@ public class GanttCalculator {
     private final Map<TacheComposite, Date> debutCalcule = new HashMap<>();
 
 
+    public Date calculerDateDebut(TacheComposite tache) {
+
+        if (debutCalcule.containsKey(tache)) {
+            return debutCalcule.get(tache);
+        }
+
+        Date debut = tache.getDateDebut();
+
+        for (TacheComposite dep : tache.getDependances()) {
+            Date debutDep = calculerDateDebut(dep);
+            Date finDep = new Date(debutDep.getTime() + dep.getDuree() * 24L * 60 * 60 * 1000);
+
+            if (finDep.after(debut)) {
+                debut = finDep;
+            }
+        }
+
+        debutCalcule.put(tache, debut);
+        return debut;
+    }
+
 }
